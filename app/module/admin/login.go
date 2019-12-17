83c93da30517ee532bced641cbafb54f6203e8de
sinnerwjj <497634174@qq.com>
2019-12-17 23:39:00 +0800

    admin

--- app/module/admin/login.go
+++ app/module/admin/login.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gogf/gf/crypto/gmd5"
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
+	"github.com/gogf/gf/os/gtime"
 	"github.com/gogf/gf/util/gvalid"
 )
 
@@ -27,7 +28,7 @@ func (c *LoginController) Login(r *ghttp.Request) {
 		"code":     r.GetForm("code", 0),
 	}
 	rule := []string{
-		"username@required|phone#用户名不能为空|请填写正确的手机号",
+		"username@required#用户名不能为空",
 		"password@required#密码不能为空",
 	}
 
@@ -35,9 +36,6 @@ func (c *LoginController) Login(r *ghttp.Request) {
 		request.ReturnJson(r, 1, e.FirstString(), nil)
 		return
 	}
-	r.Session.Set("loginUserInfo", params)
-	request.ReturnJson(r, 0, "登录成功", nil)
-	return
 	adminInfo, e := model.FindOne(adminModel, model.WithWhere(g.Map{"username": params["username"]}))
 	if e != nil {
 		request.ReturnJson(r, 1, e.Error(), nil)
@@ -56,4 +54,36 @@ func (c *LoginController) Login(r *ghttp.Request) {
 func (c *LoginController) LoginOut(r *ghttp.Request) {
 	r.Session.Remove("loginUserInfo")
 	request.ReturnJson(r, 0, "注销成功", nil)
+}
+
+/**
+重置管理员账号
+*/
+func (c *LoginController) ResetAdmin(r *ghttp.Request) {
+	admin, e := model.FindOne(adminModel, model.WithWhere(g.Map{"username": "admin"}))
+	if e != nil {
+		r.Response.Write(e)
+		return
+	}
+	password, _ := gmd5.Encrypt("123456")
+	if _, ok := admin["id"]; !ok {
+		_, e := model.Insert(adminModel, model.WithData(g.Map{
+			"username": "admin",
+			"password": password,
+			"nickname": "sinner",
+			"add_time": gtime.Now(),
+			"status":   1,
+		}))
+		if e != nil {
+			r.Response.Write(e)
+			return
+		}
+	} else {
+		_, e := model.Update(adminModel, model.WithData(g.Map{"password": password}), model.WithWhere(g.Map{"id": admin["id"]}))
+		if e != nil {
+			r.Response.Write(e)
+			return
+		}
+	}
+	r.Response.Write("重置成功")
 }
